1b5f7ade1a91a8faab73e3fe90d1fb3e3a896600
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2023-04-09 08:56:50 +0000

    feat: `tracing` OpenTelemetry emitter

--- src/logger.rs
+++ src/logger.rs
@@ -2,7 +2,12 @@
 //!
 
 use tracing::Level;
-use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
+
+use opentelemetry::sdk::export::trace::stdout;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+use tracing_subscriber::Registry;
 
 use crate::{Context, Result};
 
@@ -26,11 +31,17 @@ fn configure(level: &str) -> Result {
     #[cfg(windows)]
     let enable_ansi = false;
 
-    match tracing_subscriber::fmt()
-        .with_writer(std::io::stderr)
-        .with_max_level(level)
+    let tracer = stdout::new_pipeline().install_simple();
+    let telemetry = tracing_opentelemetry::layer::<Registry>().with_tracer(tracer);
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_writer(std::io::stderr.with_max_level(level))
         .with_span_events(FmtSpan::CLOSE)
-        .with_ansi(enable_ansi)
+        .with_ansi(enable_ansi);
+
+    match Registry::default()
+        .with(telemetry)
+        .with(fmt_layer)
         .try_init()
     {
         Err(err) => Err(anyhow!(err)),
